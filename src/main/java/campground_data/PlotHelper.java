910acc651bea391fb4daf66b912bc0ec918be931
Julius Tuba <julus3591@saskpolytech.ca>
2020-05-18 13:38:00 -0600

    PlotHelper.java

---
+++ src/main/java/campground_data/PlotHelper.java
@@ -0,0 +1,35 @@
+package campground_data;
+
+
+import java.util.ArrayList;
+
+public class PlotHelper
+{
+        private ArrayList<Plot> plots = new ArrayLIst<>();
+
+        public  PlotHelper()
+        {
+
+        }
+
+        public Plot addPlot(Plot newPlot)
+        {
+
+        }
+
+        public void removePlot(Plot plot)
+        {
+
+        }
+
+        public ArrayList<Booking> getPlotList()
+        {
+            return this.plots;
+        }
+
+        public Plot searchPlot(int plotID)
+        {
+
+        }
+}
+
