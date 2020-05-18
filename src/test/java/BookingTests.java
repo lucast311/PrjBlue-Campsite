b73ace95dae842db93b5de12bae16d7098c55dbf
Justin Waselyshen <waselyshen2334@saskpolytech.ca>
2020-05-18 14:55:07 -0600

    BookingTests Skeleton

---
+++ src/test/java/BookingTests.java
@@ -0,0 +1,90 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class BookingTests 
+{
+	//BookingHelper Tests
+	
+	@Test
+	public void testCreateBookingHelper() 
+	{
+		//return type tested will beArrayList<Booking> since this creates the list upon creation
+		
+		
+	}
+	
+	@Test
+	public void testAddBooking()
+	{
+		
+	}
+	
+	@Test
+	public void testRemoveBooking()
+	{
+		
+	}
+	
+	@Test
+	public void testChangeBookingDate()
+	{
+		
+	}
+	
+	@Test
+	public void testGetBookingListBlank()
+	{
+		
+	}
+	
+	@Test
+	public void testGetBookingListByYear()
+	{
+		
+	}
+	
+	@Test
+	public void testSearch()
+	{
+		
+	}
+	
+	//Booking Tests
+	
+	@Test
+	public void testCreateBookingWithData()
+	{
+		
+	}
+	
+	@Test
+	public void testCreateBookingBlank()
+	{
+		
+	}
+	
+	@Test
+	public void testGetters()
+	{
+		
+	}
+	
+	@Test
+	public void testSetters()
+	{
+		
+	}
+	
+	@Test
+	public void testChangers()
+	{
+		
+	}
+	
+
+}
