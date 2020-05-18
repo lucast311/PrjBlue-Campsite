b55875b7b82e2481639a349b63e9efd5e916b64a
Justin Waselyshen <waselyshen2334@saskpolytech.ca>
2020-05-18 14:11:21 -0600

    DatabaseFile skeleton

---
+++ src/main/java/campground_data/DatabaseFile.java
@@ -0,0 +1,42 @@
+package java;
+
+import java.util.ArrayList;
+
+public class DatabaseFile 
+{
+	private String filePathGuest;
+	private String filePathOwners;
+	private String filePathPlots;
+	private String filePathBookings;
+	
+	public DatabaseFile()
+	{
+		
+	}
+	
+	public ArrayList<Guest> readGuests(ArrayList<Guest> guestAcc)
+	{
+		return null;
+	}
+	
+	public ArrayList<Owner> readOwners(ArrayList<Owner> owners)
+	{
+		return null;
+	}
+	
+	public ArrayList<Plot> readPlots(ArrayList<Plot> plots)
+	{
+		return null;
+	}
+	
+	public ArrayList<Booking> readBookings(ArrayList<Booking> bookings)
+	{
+		return null;
+	}
+	
+	public void saveRecords(ArrayList list)
+	{
+		
+	}
+	
+}
