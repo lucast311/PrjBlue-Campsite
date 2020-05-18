a84a23b38bf4e7b80d89fb6a11d93008802121ef
Richie <saysana3265@saskpolytech.ca>
2020-05-18 14:10:30 -0600

    GuestHelper skeleton

---
+++ src/main/java/campground_data/GuestHelper.java
@@ -0,0 +1,41 @@
+package campground_data;
+
+import java.util.ArrayList;
+
+public class GuestHelper {
+
+    private ArrayList<Guest> guestAccounts;
+
+    public GuestHelper() {
+
+    }
+
+    public Guest addGuest(Guest newGuest) {
+
+        guestAccounts.add(newGuest);
+        return newGuest;
+
+    }
+
+    public void removeGuest(Guest guestToRemove) {
+
+        guestAccounts.remove(guestToRemove);
+
+    }
+
+    public Guest searchGuest(String phoneNumber)
+    {
+        Guest guestToReturn = null;
+
+        for (Guest guest : guestAccounts)
+        {
+            if (guest.getPhoneNumber().equals(phoneNumber))
+            {
+                guestToReturn = guest;
+            }
+        }
+
+        return guestToReturn;
+    }
+
+}
