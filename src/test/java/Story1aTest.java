074232e90e482159c2b73807b2f4caaf31e586ef
AlexanderHopkins <hopkins7681@saskpolytech.ca>
2020-05-21 10:07:51 -0600

    Story 1a unit test -Alex H

--- src/test/java/Story1aTest.java
+++ src/test/java/Story1aTest.java
@@ -17,20 +17,15 @@ public class Story1aTest {
         booking1 = new Booking();
         booking2 = new Booking(1,"0000001",new Date(2020,5,20), new Date(2020,5,22), BookingType.Cabin, 2);
 
-        assertTrue(booking1 != null && booking1 instanceof Booking);
-        assertTrue(booking2 != null && booking2 instanceof Booking);
-    }
-
-    @Test
-    public void addBookingToList()
-    {
         BookingHelper bookingHelper = new BookingHelper();
         bookingHelper.addBooking(booking1);
         bookingHelper.addBooking(booking2);
 
-        ArrayList<Booking> bookings = bookingHelper.getBookingList();
-        assertTrue(bookings.contains(booking1));
-        assertTrue(bookings.contains(booking2));
-    }
 
+        assertTrue(booking1 != null && booking1 instanceof Booking);
+        assertTrue(booking2 != null && booking2 instanceof Booking);
+
+        assertTrue(bookingHelper.getBookingList().contains(booking1));
+        assertTrue(bookingHelper.getBookingList().contains(booking2));
+    }
 }
