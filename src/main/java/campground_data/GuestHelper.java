74c988b8e03320416447278ab4e6b4c76a4e832c
RichieSaysana <saysana3265@saskpolytech.ca>
2020-05-21 11:37:33 -0600

    Implemented changeinfo method in guesthelper--Richie Saysana

--- src/main/java/campground_data/GuestHelper.java
+++ src/main/java/campground_data/GuestHelper.java
@@ -2,10 +2,13 @@ package campground_data;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class GuestHelper {
 
     private ArrayList<Guest> guestAccounts = new ArrayList<>();
+    private ValidationHelper validationHelper = new ValidationHelper();
 
     public GuestHelper() {
 
@@ -20,6 +23,63 @@ public class GuestHelper {
 
         guestAccounts.remove(guestToRemove);
 
+    }
+
+    public void changeGuestInfo(Guest guest) {
+        Scanner read = new Scanner(System.in);
+
+        System.out.print("First Name: ");
+        guest.setFirstName(read.nextLine());
+        System.out.print("Last Name: ");
+        guest.setLastName(read.nextLine());
+        System.out.print("Email: ");
+        guest.setEmail(read.nextLine());
+        System.out.print("Phone Number: ");
+        guest.setPhoneNumber(read.nextLine());
+        System.out.print("Payment Method: Select [C]redit, [D]ebit, C[A]sh");
+        switch (read.nextLine())
+        {
+            case "C":
+                guest.setPaymentMethod(PaymentType.Credit);
+                break;
+            case "D":
+                guest.setPaymentMethod(PaymentType.Debit);
+                break;
+            case "A":
+                guest.setPaymentMethod(PaymentType.Cash);
+                break;
+            default:
+                System.out.print("You selected an invalid character, no changes made to payment method.\n");
+                break;
+        }
+        System.out.print("Credit Card Number (If none enter all zeroes):");
+        guest.setCreditCardNum(read.nextLine());
+        System.out.print("Street Number: ");
+        guest.getAddress().setStreetNum(read.nextInt());
+        System.out.print("Apt Number: ");
+        guest.getAddress().setAptNum(read.nextInt());
+        System.out.print("Street Name: ");
+        guest.getAddress().setStreetName(read.nextLine());
+        System.out.print("City/Town: ");
+        guest.getAddress().setCity_Town(read.nextLine());
+        System.out.print("Province: ");
+        guest.getAddress().setProvince(read.nextLine());
+        System.out.print("Country: ");
+        guest.getAddress().setCountry(read.nextLine());
+        System.out.print("Postal Code (A1A1A1): ");
+        guest.getAddress().setPostalCode(read.nextLine());
+
+        //output errors if any otherwise output new car
+        HashMap<String,String> errors = validationHelper.getErrors(guest);
+        if(errors.size()==0) {
+            System.out.println("Success:");
+            System.out.println(guest.toString());
+        }else{
+            for(String errMsg : errors.values()) {
+                System.out.println("Errors:");
+                System.out.println(errMsg);
+            }
+        }
     }
 
     public ArrayList<Guest> getGuestAccounts() {
