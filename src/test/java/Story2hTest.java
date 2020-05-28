3d92dd85117ea5afe6f2d4e96ff8f18fb558b168
Justin Waselyshen <waselyshen2334@saskpolytech.ca>
2020-05-28 11:02:36 -0600

    Added story2htest skeleton and MainWindow javafx class -JW

---
+++ src/test/java/Story2hTest.java
@@ -0,0 +1,62 @@
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import campground_data.Booking;
+import campground_data.BookingHelper;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Story2hTest
+{
+   public BookingHelper bookingHelper=new BookingHelper();
+   public ArrayList<Booking> obList=bookingHelper.getBookingList();
+
+   @Test
+   public void testCreateViewBookingWindow()
+   {
+
+   }
+
+   @Test
+   public void testViewAll()
+   {
+
+   }
+
+    @Test
+    public void testViewCurrent()
+    {
+
+    }
+
+    @Test
+    public void testViewCabinsOnly()
+    {
+
+    }
+
+    @Test
+    public void testViewSitesOnly()
+    {
+
+    }
+
+    @Test
+    public void testGetBookingListByMonth()
+    {
+
+    }
+
+    @Test
+    public void testGetBookingListByYear()
+    {
+
+    }
+
+    @Test
+    public void testGetBookingListByMonthAndYear()
+    {
+
+    }
+}
