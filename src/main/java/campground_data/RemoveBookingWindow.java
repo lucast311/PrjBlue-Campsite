d1c72cc363415a5cae3dc0e5e13aa73ba4418932
Mel Franciz Andes <melfranciz@gmail.com>
2020-05-28 11:49:47 -0600

    Created RemoveBookingWindow and Story2bTest classes. Also added skeleton methods  M.A.

---
+++ src/main/java/campground_data/RemoveBookingWindow.java
@@ -0,0 +1,22 @@
+package campground_data;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+/**
+ * Will be creating the GUI for removing a booking
+ */
+public class RemoveBookingWindow extends Application {
+
+    public static void main(String[] args)
+    {
+        Application.launch(args);
+    }
+
+    @Override
+    public void start(Stage obStage)
+    {
+
+    }
+}
---
+++ src/test/java/Story2bTest.java
@@ -0,0 +1,35 @@
+
+
+import org.junit.Test;
+
+
+
+/**
+ * This class will contain test cases for Story 1b
+ */
+public class Story2bTest
+{
+
+
+    /**
+     * Test for removing booking object from list of bookings
+     */
+    @Test
+    public void removeBookingFromList()
+    {
+
+
+
+    }
+
+    /**
+     * Test for searching for a booking using GuestID
+     */
+    @Test
+    public void selectBookingToBeRemoved()
+    {
+
+    }
+
+
+}
