7ae9266579c0af9d8e30a0d20155bb9a3c908913
Julius Tuba <julus3591@saskpolytech.ca>
2020-05-28 11:51:52 -0600

    Changed the pom.xml file, added a MainWindow class skeleton, and a skeleton for Story2yTest -- Julius

---
+++ src/test/java/Story2yTest.java
@@ -0,0 +1,31 @@
+
+
+import campground_data.Guest;
+import campground_data.GuestHelper;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+
+
+public class Story2yTest
+{
+    private GuestHelper guestHelper = new GuestHelper();
+    private ArrayList<Guest> guests = guestHelper.getGuestAccounts();
+
+
+    @Test
+    public void testViewGuests()
+    {
+
+    }
+
+    @Test
+    public void testSearchGuest()
+    {
+
+    }
+
+}
