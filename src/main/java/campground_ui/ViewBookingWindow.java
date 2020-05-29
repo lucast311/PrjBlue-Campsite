fa71505bc2bb59e23d71399e5ec2e2eca435f602
Justin Waselyshen <waselyshen2334@saskpolytech.ca>
2020-05-29 16:24:56 -0600

    Added basic structure of ViewBookingWindow, only missing area to display results in -JW

--- src/main/java/campground_ui/ViewBookingWindow.java
+++ src/main/java/campground_ui/ViewBookingWindow.java
@@ -1,10 +1,12 @@
 package campground_ui;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class ViewBookingWindow extends Application {
@@ -17,13 +19,42 @@ public class ViewBookingWindow extends Application {
     public void start(Stage obStage) throws Exception
     {
         BorderPane obBP=new BorderPane();
-        Button obBtn=new Button("Ok");
-        obBP.setCenter(obBtn);
-        TextField obTextField=new TextField();
+        GridPane obGrid=new GridPane();
+        String[] saMonths={"January","February","March","April","May","June","July","August","September","October","November","December"};
+        String[] saYears={"2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020","2021","2022","2023","2024","2025","2026","2027","2028","2029","2030"};
 
-        obBP.setBottom(obTextField);
+        Button obCloseBtn=new Button("Close");
+        Button obViewAllBtn=new Button("View All Bookings");
+        Button obViewCurrentBtn=new Button("View Current Bookings");
+        Button obApply=new Button("Apply Filters");
 
-        obStage.setScene(new Scene(obBP,250,250));
+        Text obFilters=new Text("Filters:");
+        Text obFilterMonthYear=new Text("Filter by Month and/or Year:");
+
+        ToggleGroup obGroup=new ToggleGroup();
+        RadioButton obCabins=new RadioButton("View Cabins Only");
+        RadioButton obSites=new RadioButton("View Sites Only");
+        obCabins.setToggleGroup(obGroup);
+        obSites.setToggleGroup(obGroup);
+
+        final ComboBox cbMonth=new ComboBox(FXCollections.observableArrayList(saMonths));
+        final ComboBox cbYear=new ComboBox(FXCollections.observableArrayList(saYears));
+
+        obGrid.add(obCloseBtn,0,0);
+        obGrid.add(obViewAllBtn,0,1);
+        obGrid.add(obViewCurrentBtn,0,2);
+        obGrid.add(obFilters,0,3);
+        obGrid.add(obCabins,0,4);
+        obGrid.add(obSites,0,5);
+        obGrid.add(obFilterMonthYear,0,6);
+        obGrid.add(cbMonth,0,7);
+        obGrid.add(cbYear,0,8);
+        obGrid.add(obApply,0,9);
+        obGrid.setVgap(30);
+
+        obBP.setLeft(obGrid);
+
+        obStage.setScene(new Scene(obBP,500,500));
         obStage.setTitle("View Bookings");
         obStage.show();
     }
