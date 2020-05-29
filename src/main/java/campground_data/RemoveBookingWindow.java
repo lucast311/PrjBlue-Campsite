b72f10d753088ba2d4cf98a47d35c60b7ff3125e
Mel Franciz Andes <melfranciz@gmail.com>
2020-05-29 08:47:15 -0600

    Initialized GUI Controls and layout. Also tested that JavaFX is working M.A.

--- src/main/java/campground_data/RemoveBookingWindow.java
+++ src/main/java/campground_data/RemoveBookingWindow.java
@@ -1,6 +1,15 @@
 package campground_data;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
 
 
@@ -8,6 +17,17 @@ import javafx.stage.Stage;
  * Will be creating the GUI for removing a booking
  */
 public class RemoveBookingWindow extends Application {
+
+    private BorderPane obBPane;
+    private GridPane obGPane;
+
+    private TextFlow taBookingList;
+    private Label lblGuestID, lblbookingID, lblAccomodationID, lblStartDate, lblEndDate, lblType, lblMemberCount,
+            lblTotalPrice;
+    private TextField txtGuestID, txtbookingID, txtAccomodationID, txtStartDate, txtEndDate, txtType, txtMemberCount,
+            txtTotalPrice;
+    private Button btnRemove, btnSave, btnClose;
+
 
     public static void main(String[] args)
     {
@@ -17,6 +37,14 @@ public class RemoveBookingWindow extends Application {
     @Override
     public void start(Stage obStage)
     {
+        obBPane = new BorderPane();
 
+        btnRemove = new Button("Remove");
+
+        obBPane.setTop(btnRemove);
+
+        obStage.setScene(new Scene(obBPane, 700, 500));
+        obStage.setTitle("Cest Lake - Remove Booking");
+        obStage.show();
     }
 }
