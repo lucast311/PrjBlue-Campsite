31dc1f829a60887fc80d0ff02d7d4dc207850110
RichieSaysana <saysana3265@saskpolytech.ca>
2020-05-29 11:06:01 -0600

    Removed member count attribute for guest, realized its already a part of booking. Made the window write out to guest database file when save is clicked. Had the edit guest window also take in an int index value so that it can know exactly where the guest object is in the arraylist. --Richie

--- src/main/java/campground_data/MainTest.java
+++ src/main/java/campground_data/MainTest.java
@@ -3,11 +3,21 @@ package campground_data;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class MainTest extends Application {
-    private Pane obPane;
+    private GridPane obPane;
+    private DatabaseFile dbFile = new DatabaseFile();
+    private ArrayList<Guest> guestList = new ArrayList<>();
+    private Guest obGuest = new Guest();
+
+    private Text obText = new Text();
 
     public static void main(String[] args) {
         launch(args);
@@ -17,17 +27,37 @@ public class MainTest extends Application {
     public void start(Stage primaryStage) {
         Button btnTest = new Button();
         btnTest.setText("Test");
-        obPane = new Pane();
 
-        obPane.getChildren().add(btnTest);
+        Button btnRefresh = new Button();
+        btnRefresh.setText("Refresh");
 
-        Guest obGuest = new Guest("Richie", "Saysana", "Email", "PhoneNumber", PaymentType.Cash, "CreditCard",
-                4, new Address(123, 123, "StreetName", "City", "State", "Country", "A1A1A1"));
+        obPane = new GridPane();
+
+        obPane.add(btnTest,0 ,0);
+        obPane.add(btnRefresh,1,0);
+
+        int nIndex = 0;
+
+        guestList = dbFile.readGuests();
+        obGuest = guestList.get(nIndex);
+        obText.setText(obGuest.toString());
+        obPane.add(obText, 1, 1);
 
         btnTest.setOnAction(e -> {
-            EditGuestWindow guestWindow = new EditGuestWindow(obGuest);
+            guestList = dbFile.readGuests();
+            obGuest = guestList.get(nIndex);
+            EditGuestWindow guestWindow = new EditGuestWindow(obGuest, nIndex);
+            guestWindow.initModality(Modality.WINDOW_MODAL);
+            guestWindow.initOwner(primaryStage);
 
             guestWindow.show();
+        });
+
+        btnRefresh.setOnAction(e -> {
+            ArrayList<Guest> guestList2 = dbFile.readGuests();
+            obPane.getChildren().remove(obText);
+            this.obText.setText(guestList2.get(0).toString());
+            obPane.add(this.obText, 1, 1);
         });
 
         primaryStage.setScene(new Scene(obPane, 800, 800));
