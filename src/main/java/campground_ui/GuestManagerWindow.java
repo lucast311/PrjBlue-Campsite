85746ba2623a361bd45f16101e8be0fff7ab0501
Julius Tuba <julus3591@saskpolytech.ca>
2020-06-01 10:10:55 -0600

    Made changes so that the when the window opens, it's full screen to fit everything and broke down the address into its individual attributes --Julius

--- src/main/java/campground_ui/GuestManagerWindow.java
+++ src/main/java/campground_ui/GuestManagerWindow.java
@@ -7,6 +7,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
 import javafx.scene.layout.BorderPane;
@@ -19,7 +20,7 @@ public class GuestManagerWindow extends Application
     private BorderPane borderPane = new BorderPane();
     private GridPane guestsPane = new GridPane();
     private ArrayList<Guest> guests = new ArrayList<>();
-    private Text txtSearch = new Text("Search");
+    private Label lblSearch = new Label("Search");
     private TextField txtSearchField = new TextField();
     private HBox topBox = new HBox();
     private HBox searchBox = new HBox();
@@ -38,20 +39,25 @@ public class GuestManagerWindow extends Application
     public void start(Stage primaryStage)
     {
 
+
+        txtSearchField.setId("txtSearchField");
+        txtSearchField.setPromptText("Enter a guest's phone number");
+
         searchBox.setSpacing(10);
-        searchBox.getChildren().addAll(txtSearch,txtSearchField);
+        searchBox.getChildren().addAll(lblSearch,txtSearchField);
 
         topBox.setAlignment(Pos.CENTER);
         topBox.setPadding(new Insets(50, 20, 50, 20));
-        topBox.setSpacing(100);
+        topBox.setSpacing(1300);
         topBox.getChildren().addAll(searchBox, btnBack);
 
         buttonsBox.setAlignment(Pos.CENTER);
-        buttonsBox.setSpacing(50);
+        buttonsBox.setSpacing(200);
         buttonsBox.setPadding(new Insets(50, 0, 50, 0));
         buttonsBox.getChildren().addAll(btnAddGuest,btnEditGuest);
 
-        guestsPane.setAlignment(Pos.CENTER);
+        guestsPane.setAlignment(Pos.TOP_CENTER);
+        guestsPane.setStyle("-fx-border-color: black");
 
         borderPane.setCenter(guestsPane);
         borderPane.setTop(topBox);
@@ -73,9 +79,8 @@ public class GuestManagerWindow extends Application
             }
         }
 
-
-
         primaryStage.setScene(new Scene(borderPane, 400, 400));
+        primaryStage.setMaximized(true);
         primaryStage.setTitle("Guest Manager");
         primaryStage.show();
     }
@@ -83,28 +88,38 @@ public class GuestManagerWindow extends Application
 
     public void setHeader(GridPane gridPane)
     {
-        gridPane.add(new Text("Guest ID\t"), 0, 0);
-        gridPane.add(new Text("First Name\t"), 1, 0);
-        gridPane.add(new Text("Last Name\t"), 2, 0);
-        gridPane.add(new Text("Email\t"), 3, 0);
-        gridPane.add(new Text("Phone Number\t"), 4, 0);
+        gridPane.add(new Text("Guest ID\t\t"), 0, 0);
+        gridPane.add(new Text("First Name\t\t"), 1, 0);
+        gridPane.add(new Text("Last Name\t\t"), 2, 0);
+        gridPane.add(new Text("Email\t\t"), 3, 0);
+        gridPane.add(new Text("Phone Number\t\t"), 4, 0);
         gridPane.add(new Text("Payment Method\t"), 5, 0);
-        gridPane.add(new Text("Credit Card Number\t"), 6, 0);
-        gridPane.add(new Text("Address\t"), 7, 0);
+        gridPane.add(new Text("Credit Card Number\t\t"), 6, 0);
+        gridPane.add(new Text("Street Number\t\t"), 7, 0);
+        gridPane.add(new Text("Apt Number\t\t"), 8, 0);
+        gridPane.add(new Text("Street Name\t\t"), 9, 0);
+        gridPane.add(new Text("City/Town\t\t"), 10, 0);
+        gridPane.add(new Text("Province\t\t"), 11, 0);
+        gridPane.add(new Text("Country\t\t"), 12, 0);
+        gridPane.add(new Text("Postal Code\t\t"), 13, 0);
     }
 
     public void addRow(GridPane gridPane, Guest obGuest, int nRow)
     {
-
             gridPane.add(new Text(String.valueOf(obGuest.getGuestID())), 0, nRow);
             gridPane.add(new Text(obGuest.getFirstName()), 1, nRow);
             gridPane.add(new Text(obGuest.getLastName()), 2, nRow);
-            gridPane.add(new Text(obGuest.getEmail()), 3, nRow);
+            gridPane.add(new Text(obGuest.getEmail() + "\t\t"), 3, nRow);
             gridPane.add(new Text(obGuest.getPhoneNumber()), 4, nRow);
             gridPane.add(new Text(String.valueOf(obGuest.getPaymentMethod())), 5, nRow);
             gridPane.add(new Text(obGuest.getCreditCardNum()), 6, nRow);
-            gridPane.add(new Text(obGuest.getAddress().toString()), 7, nRow);
-
+            gridPane.add(new Text(String.valueOf(obGuest.getAddress().getStreetNum()) + "\t\t"), 7, nRow);
+            gridPane.add(new Text(String.valueOf(obGuest.getAddress().getAptNum()) + "\t\t"), 8, nRow);
+            gridPane.add(new Text(obGuest.getAddress().getStreetName() + "\t\t"), 9, nRow);
+            gridPane.add(new Text(obGuest.getAddress().getCity_Town() + "\t\t"), 10, nRow);
+            gridPane.add(new Text(obGuest.getAddress().getProvince() + "\t\t"), 11, nRow);
+            gridPane.add(new Text(obGuest.getAddress().getCountry() + "\t\t"), 12, nRow);
+            gridPane.add(new Text(obGuest.getAddress().getPostalCode() + "\t\t"), 13, nRow);
     }
 
 
