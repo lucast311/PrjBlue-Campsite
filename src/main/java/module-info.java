2a7082769348f62a1f6f10d356677224d879a0e6
Mel Franciz Andes <melfranciz@gmail.com>
2020-06-01 09:13:25 -0600

    Minor module-infor change M.A.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module campground_ui
+{
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires java.validation;
+    requires json.simple;
+    exports campground_ui;
+    exports campground_data;
+}
